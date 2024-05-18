e58f5be3ab7fafdb5c6262f54aec5bf4767fbd21
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-05-18 08:56:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const contents = document.querySelectorAll('.content')
 const listItems = document.querySelectorAll('nav ul li')
-
+ 
 listItems.forEach((item, idx) => {
     item.addEventListener('click', () => {
         hideAllContents()
